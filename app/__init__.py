ff658294ff73c380a1ea93b4cac5e6f4b19d6977
Yijun Lu <lanc291267776@gmail.com>
2024-10-30 15:01:49 +1100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ jwt = JWTManager()
 def create_app():
     app = Flask(__name__)
     app.config['JWT_SECRET_KEY'] = 'testonlyfornow'  # use secret key to replace'testonlyfornow'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5703@localhost:5432/5703'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:5703@localhost:5432/5703' # postgresql://username:userpassword@localhost:5432/databasename
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
